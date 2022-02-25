aa2c4cc69065d23d117a34b1e5b49404a8758dc8
djolodjolo <tkd_djole@rocketmail.com>
2022-02-25 17:43:38 +0100

    Added roomModel

--- db.js
+++ db.js
@@ -7,11 +7,11 @@ mongoose.connect(mongoURL, {useUnifiedTopology : true, useNewUrlParser : true})
 var connection = mongoose.connection
 
 connection.on('error', () => {
-    console.log('Mongo DB connection failed')
+    console.log('Mongo DB connection failed') /*Ispis ako nije uspjesna konekcija na bazu */
 })
 
 connection.on('connected', () => {
-    console.log('Mongo DB connection successful')
+    console.log('Mongo DB connection successful') /*Ispise na konzolu ako je uspijesna konekcija na bazu */
 })
 
 module.exports = mongoose
