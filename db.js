ec49a620d1c5c5b13adc1d68a4c3892f29c4c0bc
djolodjolo <tkd_djole@rocketmail.com>
2022-02-25 17:50:15 +0100

    Added roomModel

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-var mongoURL = 'mongodb+srv://djole:<password>@cluster0.4nntf.mongodb.net/mern-apartments'
+var mongoURL = 'mongodb+srv://djole:08031993@cluster0.4nntf.mongodb.net/mern-apartments'
 
 mongoose.connect(mongoURL, {useUnifiedTopology : true, useNewUrlParser : true})
 
