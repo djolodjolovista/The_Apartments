e47678827ce11fbbf8d1e6809fa54a4db4863fba
djolodjolo <tkd_djole@rocketmail.com>
2022-03-02 17:18:49 +0100

    Added loginscreen and registerscreen fronted

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Navbar from './components/Navbar';
 import {BrowserRouter, Route, Link} from 'react-router-dom';/**BrowserRouter-It is the parent component that is used to store all of the other components. */
 import Homescreen from './screens/Homescreen';
 import Bookingscreen from './screens/Bookingscreen';
+import Registerscreen from './screens/Registerscreen';
+import Loginscreen from './screens/Loginscreen';
 /**Route- Рута је условно приказана компонента заснована на подударању путање до УРЛ-а. */
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
      
      <Route path='/home' exact component={Homescreen} />
      <Route path='/book/:roomid' exact component={Bookingscreen} />
+     <Route path='/register' exact component={Registerscreen} />
+     <Route path='/login' exact component={Loginscreen} />
      </BrowserRouter>
     </div>
   );
