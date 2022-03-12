cd9b354eee23ec39215549614949ed0feff95910
djolodjolo <tkd_djole@rocketmail.com>
2022-03-12 22:39:31 +0100

    Add new room frontend

--- client/src/screens/Adminscreen.js
+++ client/src/screens/Adminscreen.js
@@ -28,7 +28,7 @@ function Adminscreen() {
       <Rooms />
     </TabPane>
     <TabPane tab="Add Room" key="3">
-      <h1>Add Room</h1>
+      <AddRoom />
     </TabPane>
     <TabPane tab="Users" key="4">
       <Users />
@@ -40,6 +40,7 @@ function Adminscreen() {
 
 export default Adminscreen;
 
+//Bookings component (mogli smo i napraviti u folderu components pa importovati)
 export function Bookings() {
 
     const [bookings, setbookings] = useState([])
@@ -108,6 +109,7 @@ export function Bookings() {
     )
 }
 
+//Rooms component
 export function Rooms() {
 
   const [rooms, setrooms] = useState([])
@@ -176,6 +178,7 @@ export function Rooms() {
   )
 }
 
+//Users component
 export function Users(){
   const [users, setusers] = useState([])
   const [loading, setloading] = useState(true)
@@ -236,3 +239,69 @@ return(
 );
 
 }
+
+//Add Room component
+
+
+export function AddRoom() {
+
+  const [name, setname] = useState('')
+  const [rentperday, setrentperday] = useState()
+  const [maxcount, setmaxcount] = useState()
+  const [description, setdescription] = useState()
+  const [phonenumber, setphonenumber] = useState()
+  const [type, settype] = useState()
+  const [imageurl1, setimageurl1] = useState()
+  const [imageurl2, setimageurl2] = useState()
+  const [imageurl3, setimageurl3] = useState()
+
+  function addRoom(){
+    const newroom = {
+      name,
+      rentperday,
+      maxcount,
+      description,
+      phonenumber,
+      type,
+      imageurls: [imageurl1,imageurl2,imageurl3]
+    }
+    console.log(newroom)
+  }
+
+  return (
+    <div className='row'>
+      <div className='col-md-5'>
+        <input type='text' className='form-control' placeholder='room name'
+        value={name} onChange={(e)=>{setname(e.target.value)}}/>
+        <input type='text' className='form-control' placeholder='rent per day'
+        value={rentperday} onChange={(e)=>{setrentperday(e.target.value)}}/>
+        <input type='text' className='form-control' placeholder='max count'
+        value={maxcount} onChange={(e)=>{setmaxcount(e.target.value)}}/>
+        <input type='text' className='form-control' placeholder='description'
+        value={description} onChange={(e)=>{setdescription(e.target.value)}}/>
+        <input type='text' className='form-control' placeholder='phone number'
+        value={phonenumber} onChange={(e)=>{setphonenumber(e.target.value)}}/>
+
+      </div>
+      <div className='col-md-5'>
+      <input type='text' className='form-control' placeholder='type'
+      value={type} onChange={(e)=>{settype(e.target.value)}}/>
+      <input type='text' className='form-control' placeholder='Image URL 1'
+      value={imageurl1} onChange={(e)=>{setimageurl1(e.target.value)}}/>
+      <input type='text' className='form-control' placeholder='Image URL 2'
+      value={imageurl2} onChange={(e)=>{setimageurl2(e.target.value)}}/>
+      <input type='text' className='form-control' placeholder='Image URL 3'
+      value={imageurl3} onChange={(e)=>{setimageurl3(e.target.value)}}/>
+
+      <div className='text-right'>
+        <button className='btn btn-primary mt-2' onClick={addRoom}>Add Room</button>
+
+      </div>
+
+      </div>
+      
+
+    </div>
+  )
+}
+
